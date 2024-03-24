df2d96a9a21b97c05d499e832b80af9739cbe48e
Luiz Arlochi <luiz.arlochi@gmail.com>
2024-03-23 22:21:28 -0300

    fix vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/Wallet-Currency-Converter",
+  base: "/Wallet-Currency-Converter",
+  build: {
+    chunkSizeWarningLimit: 3000,
+  }
 })
